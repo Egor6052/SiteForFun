45dd2a9dc10b8508959d8f9fdab53fc75b5f5df9
Yegor <segor6052@gmail.com>
2024-08-16 11:53:37 +0300

    Add draft

---
+++ site/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <MainFolder msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import MainFolder from './components/MainFolder.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainFolder
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ site/src/components/MainFolder.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="Main">
+     <div class="header">
+      <ul>
+        <li><div class="button-back"><a class="button-back" href="">Back</a></div></li>
+        <li><div class="Text">Wallet</div></li>
+        <li><div class="button-more"><a class="button-more" href="">...</a></div></li>
+      </ul>
+      
+      
+     </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // name: 'HelloWorld',
+  // props: {
+  //   msg: String
+  // }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  @import '../assets/css/Main.css';
+</style>
