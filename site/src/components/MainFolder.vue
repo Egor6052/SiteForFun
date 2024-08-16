d7e802801781bc7f8c5989352a3aa55e94358375
Yegor <segor6052@gmail.com>
2024-08-16 13:36:11 +0300

    Add Main component

--- site/src/components/MainFolder.vue
+++ site/src/components/MainFolder.vue
@@ -2,12 +2,48 @@
   <div class="Main">
      <div class="header">
       <ul>
-        <li><div class="button-back"><a class="button-back" href="">Back</a></div></li>
+        <li><div class="button-back"><a class="button-back" href=""><img src="../assets/img/back.png" alt="">Back</a></div></li>
         <li><div class="Text">Wallet</div></li>
-        <li><div class="button-more"><a class="button-more" href="">...</a></div></li>
+        <li><div class="button-more"><a class="button-more" href=""><img src="../assets/img/more.png" alt=""></a></div></li>
       </ul>
-      
-      
+     </div>
+
+
+     <div class="content">
+      <div class="prise">
+
+        <div class="ticket">
+          <img src="../assets/img/ticket.png" alt="">
+        </div>
+        <div class="baner">
+          <ul>
+            <li><a class="buy-ticket" href=""><p>Buy ticket</p></a></li>
+            <li><div class="plus-minus"><a class="plus" href=""><p>+</p></a><a class="minus" href=""><p>-</p></a></div></li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="list">
+        <ul>
+          <li><a class="point" href=""></a><p>Friends name</p></li>
+          <li><a class="point" href=""></a><p>Friends name</p></li>
+          <li><a class="point" href=""></a><p>Friends name</p></li>
+          <li><a class="point" href=""></a><p>Friends name</p></li>
+        
+        
+        </ul>
+      </div>
+     </div>
+
+
+     <div class="bar">
+      <ul>
+        <li><div class="button1"><a href=""></a></div></li>
+        <li><div class="button2"><a href=""></a></div></li>
+        <li><div class="button3"><a href=""></a></div></li>
+        <li><div class="button4"><a href=""></a></div></li>
+        <li><div class="button5"><a href=""></a></div></li>
+      </ul>
      </div>
   </div>
 </template>
